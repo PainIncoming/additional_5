b1453bafdb38dde75161b0ce8d952a5d9cc2cf8f
PainIncoming <galko3.v@gmail.com>
2018-09-16 16:27:17 +0300

    Implement function 'check'

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  if (str.length % 2 !== 0) return false;
+
+  do {
+    let tempStr = str;
+
+    bracketsConfig.forEach(brackets => {
+      let regExp = parseInt(brackets[0]) ?
+          new RegExp(`${brackets[0]}${brackets[1]}`, 'g') :
+          new RegExp(`\\${brackets[0]}\\${brackets[1]}`, 'g');
+
+      str = str.replace(regExp, '');
+    });
+
+    if (str.length === 0) {
+      return true;
+    } else if (str === tempStr) {
+      return false;
+    }
+  } while (true);
+};
